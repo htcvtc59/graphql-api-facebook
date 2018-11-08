a46943a5c6a35640c6e12f96e37189306484e2c2
Oh <oh@huong-pc.fis.vn>
2018-11-08 13:56:19 +0700

    first get /

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var request = require('request');
+var bodyParser = require('body-parser');
+
+var app = express();
+var jsonParser = bodyParser.json();
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+
+app.get('/', jsonParser, function (req, res) {
+    res.send("Hello Would!");
+});
+
+app.listen(3000);
